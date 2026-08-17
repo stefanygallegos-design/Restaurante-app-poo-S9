a1d08b83b44e05fcdb08ee6483f2bd86ddffce3a
Stefany Gallegos <stefanyzari@gmail.com>
2026-08-16 23:09:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-
+OPCIONES_MENU = (
+    ("1", "Registrar producto"),
+    ("2", "Buscar producto"),
+    ("3", "Actualizar producto"),
+    ("4", "Eliminar producto"),
+    ("5", "Listar productos"),
+    ("6", "Registrar usuario"),
+    ("7", "Listar usuarios"),
+    ("8", "Mostrar categorías"),
+    ("9", "Salir"),
+)
